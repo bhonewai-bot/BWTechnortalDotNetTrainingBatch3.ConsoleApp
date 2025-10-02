050edff9d58767d4221ed25d040e9c87f674bc7a
Bhone Wai <damenaosan@gmail.com>
2025-10-02 19:36:06 +0630

    feat: C# basic

--- TechnortalDotNetTrainingBatch3.ConsoleApp/Program.cs
+++ TechnortalDotNetTrainingBatch3.ConsoleApp/Program.cs
@@ -1,13 +1,103 @@
 ﻿using TechnortalDotNetTrainingBatch3.ConsoleApp;
 
-Student student = new Student(1, "Bhone Wai", new DateTime(2002, 11, 21));
+/*Student student = new Student(1, "Bhone Wai", new DateTime(2002, 11, 21));
 Console.WriteLine($"Birth date {student.DateOfBirth}");
-// student.StudentId = 1;
-// student.StudentName = "John Doe";
-// student.FatherName = "Richard Roe";
-// student.DateOfBirth = new DateTime(2002, 11, 21);
+student.StudentId = 1;
+student.StudentName = "John Doe";
+student.FatherName = "Richard Roe";
+student.DateOfBirth = new DateTime(2002, 11, 21);
 
-// Student student2 = new Student();
-// student2.StudentId = 2;
-// student2.StudentName = "John Doe 2";
-// student2.FatherName = "Richard Roe 2";
+Student student2 = new Student();
+student2.StudentId = 2;
+student2.StudentName = "John Doe 2";
+student2.FatherName = "Richard Roe 2";
+
+Wallet wallet = new Wallet();
+wallet.Name = "Bhone Wai";
+wallet.MobileNo = "800932347";
+wallet.amount = 6900;
+wallet.SetBalance(wallet.amount);
+
+
+Console.WriteLine(wallet.Balance);*/
+
+string writeText = "Hello World";
+File.WriteAllText("file.txt", writeText);
+
+Console.ReadLine();
+
+/*EnumUserType userType = EnumUserType.Guest;
+switch (userType)
+{
+    
+    case EnumUserType.Guest:
+        break;
+    case EnumUserType.User:
+        break;
+    case EnumUserType.Admin:
+        break;
+    case EnumUserType.SuperAdmin:
+        break;
+    case EnumUserType.None:
+    default:
+        break;
+}*/
+
+public abstract class Animal
+{
+    public abstract void AnimalSound();
+
+    public void Sleep()
+    {
+        Console.WriteLine("Zzz");
+    }
+}
+
+public class Cow : Animal
+{
+    public override void AnimalSound()
+    {
+        Console.WriteLine("Boob");
+    }
+}
+
+public interface ITransfer
+{
+    void Transfer(string fromMobileNo, string toMobileNo, decimal amount);
+    void TransactionHistory(string mobileNo);
+}
+
+public class Wallet : ITransfer
+{
+    public void Transfer(string fromMobileNo, string toMobileNo, decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TransactionHistory(string mobileNo)
+    { 
+        throw new NotImplementedException();
+    }
+}
+
+public class Bank : ITransfer
+{
+    public void Transfer(string fromMobileNo, string toMobileNo, decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TransactionHistory(string mobileNo)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public enum EnumUserType
+{
+    None,
+    Guest,
+    User,
+    Admin,
+    SuperAdmin
+}
--- TechnortalDotNetTrainingBatch3.ConsoleApp/Student.cs
+++ TechnortalDotNetTrainingBatch3.ConsoleApp/Student.cs
@@ -24,3 +24,39 @@ public class Father
         Console.WriteLine("Punch");
     }
 }
+
+public class Employee
+{
+    public string Name;
+    
+    public int EmployeeNo { get; set; }
+    
+    public string EmployeeName { get; set; }
+    
+    public DateTime DateOfBirth { get; set; }
+}
+
+public class Wallet
+{
+    public decimal amount;
+    public string Name { get; set; }
+    
+    public string MobileNo { get; set; }
+
+    public decimal Balance { get; private set; }
+
+    public decimal ShowBalance()
+    {
+        return Balance;
+    }
+
+    public void SetBalance(decimal amount)
+    {
+        if (amount < 0)
+        {
+            throw new Exception("Amount must be greater than 0");
+        }
+
+        Balance = amount;
+    }
+}
